4439acc1ace9a7e37e29126c3e853bf56cf37eb3
RISHI <rishi.kumar@anandorja.com>
2022-11-03 16:19:05 +0530

    Update MyThread.java

--- rishi/src/main/java/com/anandorja/learn/MyThread.java
+++ rishi/src/main/java/com/anandorja/learn/MyThread.java
@@ -22,7 +22,31 @@ class RunnableDemo implements Runnable {
         }
         System.out.println("Thread " +  threadName + " exiting.");
     }
+}
+
+class ThreadDemo extends Thread {
+    private String threadName;
+
+    ThreadDemo(String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
     }
+
+    @Override
+    synchronized public void run() {
+        System.out.println("Running " +  threadName );
+
+        try {
+            for(int i = 1; i < 6; i++) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+}
 
 
 public class MyThread {
@@ -32,8 +56,11 @@ public class MyThread {
         Thread t = new Thread(R1);
         t.start();
 
-        RunnableDemo R2 = new RunnableDemo("Thread-2");
+        /**RunnableDemo R2 = new RunnableDemo("Thread-2");
         Thread t1 = new Thread(R2);
-        t1.start();
+        t1.start();*/
+
+        ThreadDemo T1 = new ThreadDemo("Thread-2");
+        T1.start();
     }
 }
