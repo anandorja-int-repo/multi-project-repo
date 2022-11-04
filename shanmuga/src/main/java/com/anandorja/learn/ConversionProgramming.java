19c7165e51394da76814b7569db6b017018c2e37
spshanmuga01 <spshanmuga01@gmail.com>
2022-11-04 19:39:54 +0530

    Conversion Programs with junit

--- shanmuga/src/main/java/com/anandorja/learn/ConversionProgramming.java
+++ shanmuga/src/main/java/com/anandorja/learn/ConversionProgramming.java
@@ -7,11 +7,93 @@ public class ConversionProgramming {
         int i = 0;
         while (binary > 0) {
             int rem = binary % 10;
-            decimal = decimal+rem * (int) (Math.pow(2, i++));
+            decimal = decimal + rem * (int) (Math.pow(2, i++));
             binary = binary / 10;
         }
         return decimal;
     }
 
+    public int binaryToOctal(int binary) {
+        int decimal = 0, octal = 0, i = 0;
+        while (binary != 0) {
+            int remainder = binary % 2;
+            decimal = decimal + remainder * (int) (Math.pow(2, i++));
+            binary = binary / 10;
+        }
+        i = 1;
+        while (decimal > 0) {
+            int remainder = decimal % 10;
+            octal = octal + remainder * i;
+            i = i * 10;
+            decimal = decimal / 8;
+        }
+        return octal;
+    }
+
+    public String decimalToOctal(int decimal) {
+        int rem;
+        String octal = "";
+        char octalChars[] = {'0', '1', '2', '3', '4', '5', '6', '7'};
+
+        while (decimal > 0) {
+            rem = decimal % 8;
+            octal = octalChars[rem] + octal;
+            decimal = decimal / 8;
+        }
+        return octal;
+    }
+    public int decimalToHexa(int decimal) {
+        int temp;
+        int rem;
+        int i, j = 0;
+        char hexa[] = new char[100];
+        temp = decimal;
+        while (temp < 10) {
+            rem = decimal % 16;
+            if (rem < 10) {
+                hexa[j++] = (char) (48 + rem);
+            } else {
+                hexa[j++] = (char) (55 + rem);
+            }
+            temp = temp / 16;
+        }
+
+        for(i=j-1;i>=0;i--){
+            return hexa[i];
+        }
+        return hexa[i];
+    }
+    public String booleanToString(boolean bool){
+
+        bool = false;
+        String str = Boolean.toString(bool);
+        return str;
+    }
+    public Double stringToDouble(String str){
+        Double str1 = new Double(str);
+        return str1;
+    }
+    public String doubleToString(double doub){
+        String doub1 = Double.toString(doub);
+        return doub1;
+    }
+    public Long stringToLong(String str){
+        long number = new Long(str);
+        return number;
+    }
+    public String longToString(long number){
+        String str = Long.toString(number);
+        return str;
+    }
+    public char intToChar(int num){
+        char c;
+        c = (char)(num);
+        return c;
+    }
+    public int charToInt(char a){
+        int num;
+        num = a - '0';
+        return num;
+    }
 }
 
--- shanmuga/src/test/java/com/anandorja/learn/ConversionProgrammingTest.java
+++ shanmuga/src/test/java/com/anandorja/learn/ConversionProgrammingTest.java
@@ -4,8 +4,37 @@ import junit.framework.TestCase;
 
 public class ConversionProgrammingTest extends TestCase {
     ConversionProgramming result = new ConversionProgramming();
-    public void testBinaryToOctal(){
+    public void testBinaryToDecimal()
+    {
         assertEquals(10,result.binaryToDecimal(1010));
     }
-
+public void testBinaryToOctal(){
+        assertEquals(51,result.binaryToOctal(101001));
+}
+public void testDecimalToOctal(){
+        assertEquals("257",result.decimalToOctal(175));
+}
+//public void testDecimalTohexa(){
+//        assertEquals("4C",result.decimalToHexa(76));
+//}
+public void testBooleanToString(){
+        assertEquals("false",result.booleanToString(false));
+}
+public void testStringToDouble(){
+        assertEquals(7687.98765,result.stringToDouble("7687.98765"));
+}
+public void testDoubleToString(){
+        assertEquals("764354.9843",result.doubleToString(764354.9843));
+}
+//public void testStringToLong() {
+//    assertEquals(999999999999, result.stringToLong("999999999999"));
+public void testLongToString(){
+    assertEquals("999999999",result.longToString(999999999));
+    }
+public void testIntToChar(){
+        assertEquals('B',result.intToChar(66));
+}
+public void testCharToInt(){
+        assertEquals(3,result.charToInt('3'));
+}
 }
